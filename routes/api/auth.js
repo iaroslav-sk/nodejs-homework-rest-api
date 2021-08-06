883368d649bf8c54aa86523af64f27bbf1c23003
iaroslav-sk <iaroslav.dev.sk@gmail.com>
2021-08-06 15:06:54 +0300

    add avatars download methdots (first done)

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,12 +1,46 @@
 const { Router } = require("express");
+const { v4: uuidv4 } = require("uuid");
+const path = require("path");
+const fs = require("fs").promises;
 
 const jwtMiddelware = require("./middlewares/jwtToken.middleware");
-
+// const {
+//   uploadMiddelware,
+//   uploadDir,
+// } = require("./middlewares/avatarMiddelvare");
 const { auth: ctrl } = require("./controller");
 
 const router = Router();
 
-router.post("/register", ctrl.register);
+router.post(
+  "/register",
+  // uploadMiddelware.single("avatar"),
+  // async (req, res, next) => {
+  //   console.log(req.file);
+  //   const { path: tempName, originalname } = req.file;
+  //   const userId = uuidv4();
+  //   const useDirectory = path.join(uploadDir, userId);
+  //   try {
+  //     await fs.mkdir(useDirectory);
+  //     const fileName = path.join(useDirectory, originalname);
+  //     fs.rename(tempName, fileName);
+  //     const newUser = {
+  //       _id: userId,
+  //       avatar: fileName,
+  //     };
+  //     res.status(201).json({
+  //       status: "success",
+  //       code: 201,
+  //       data: {
+  //         result: newUser,
+  //       },
+  //     });
+  //   } catch (error) {
+  //     fs.unlink(tempName);
+  //   }
+  // },
+  ctrl.register
+);
 
 router.post("/login", ctrl.login);
 
