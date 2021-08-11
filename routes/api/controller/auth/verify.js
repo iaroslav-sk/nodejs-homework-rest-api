b0ea45374fbaec0960fa758a8d765a466b679d3c
iaroslav-sk <iaroslav.dev.sk@gmail.com>
2021-08-11 12:24:24 +0300

    fix veryfy.js methods change verifyToken

--- routes/api/controller/auth/verify.js
+++ routes/api/controller/auth/verify.js
@@ -12,10 +12,10 @@ const verify = async (req, res) => {
         .status(HTTP_CODES.UNAUTHORIZED)
         .json({ error: "Not Found, verify failed" });
     }
-    // await User.findOneAndUpdate(
-    //   { _id: candidate._id },
-    //   { verifyToken: null, verify: true }
-    // );
+    await User.findOneAndUpdate(
+      { _id: candidate._id },
+      { verifyToken: null, verify: true }
+    );
     res.status(HTTP_CODES.OK).json("Verification successful");
   } catch (error) {
     res.status(HTTP_CODES.BAD_REQUEST).json({ error: error.message });
